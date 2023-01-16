7b47a58c65d8627ea9a7ec1cc52a101c71234c54
ton1k0 <domarevsasha985@gmail.com>
2023-01-16 16:08:33 +0300

    swagger

--- Ginterest/settings.py
+++ Ginterest/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_swagger',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'Ginterest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',
+        'USER': 'postgres',
+        'PASSWORD': '20041014',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
